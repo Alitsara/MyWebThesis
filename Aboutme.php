d8c740bafa07e8995cfc6d955eefeb1fd39c8adc
Alitsara <111361950+Alitsara@users.noreply.github.com>
2024-02-12 11:38:58 +0700

    Add files via upload

---
+++ Aboutme.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+</head>
+<body>
+    <div id = "about">
+    <center>
+        <h5>ระบบช่วยตัดสินใจเลือกพฤติกรรมการใช้เวลาว่างสำหรับผู้สูงอายุด้วยเทคนิคดาต้าไมนิ่ง</h5><br>
+        <h4>จัดทำโดย</h4><br>
+        <h4>นางสาวอริสรา ม่วงขำ ชั้นปีที่ 4 คณะวิทยาศาสตร์ สาขาวิทยาการคอมพิวเตอร์</h4><br><br>
+        <h4>เว็บแอพพลิเคชั่นนี้จัดทำขึ้นเพื่อเป็นส่วนหนึ่งของวิทยานิพนธ์</h4>
+
+    </center>
+    </div>
+</body>
+</html>
